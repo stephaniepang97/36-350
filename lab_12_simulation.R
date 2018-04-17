6ac8f1e5829b3cfdf6b7c83babe5196481a24e67
stephaniepang <spang@andrew.cmu.edu>
2018-04-17 12:32:04 -0400

    Part 2b

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -5,3 +5,17 @@ generate_data = function(n, p){
     responses=rnorm(n)
   ))
 }
+
+# 2b
+model_select = function(covariates, responses, cutoff){
+  out = lm(responses~covariates)
+  p.values = summary(out)$coefficients[,4]
+  retained = which(p.values <= cutoff)
+  if (length(retained) == 0 || all(retained == c(1))){
+    return(c())
+  } 
+  
+  # subtracting 1 to account for intercept 
+  retained.out = lm(responses ~ covariates[,retained-1])
+  return(summary(retained.out)$coefficients[,4])
+}
